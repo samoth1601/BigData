b6ff43f63739ec4c5e76b04c61b193497d6715e9
jmfilberg <jmfilberg@gmail.com>
2016-04-18 18:48:35 +0200

    Laga lokal tid lagt til ()Noe gøy

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,12 @@ import org.apache.spark.api.java.*;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.function.Function;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
 
@@ -16,7 +22,7 @@ public class App {
          */
 
         /**1. Load the Foursquare dataset.*/
-        String logFile = "./src/main/resources/dataset_TIST2015.tsv"; // Should be some file on your system
+        String logFile = "C:/Users/jonmartin/Desktop/BigData/Foursquare_data/dataset_TIST2015.tsv"; // Should be some file on your system
         SparkConf conf = new SparkConf().setAppName("Simple Application").setMaster("local");;
         JavaSparkContext sc = new JavaSparkContext(conf);
         //JavaRDD<String> logData = sc.textFile(logFile).cache();
@@ -24,20 +30,41 @@ public class App {
         //JavaRDD<Integer> lineLengths = lines.map(s -> s.length());
         //int totalLength = lineLengths.reduce((a, b) -> a + b);
 
-
-        /** 2. Calculate local time for each check-in (UTC time + timezone offset).*/
-
-
+        //List<String> sortedLines = lines.takeOrdered(lines.count());
+        List<String> sortedLines = lines.takeOrdered((int) lines.count()/10);
+        for (String line : sortedLines) {
+            String[] splittedLine = line.split("\t");
+            String dateTime = splittedLine[3];
+            String timeOffset = splittedLine[4];
+            //System.out.println(time + " " + timeOffset);
+            calculateLocalTime(dateTime, timeOffset);
+            //System.out.println(dateTime + " " + timeOffset);
+        }
+        //System.out.println(sortedLines);
+        System.out.println("heeeeeeeeeeeey");
+        /** 2. Calculate local time for each check-in (UTC time + timezone offset).
 
         long numAs = lines.filter(new Function<String, Boolean>() {
             public Boolean call(String s) { return s.contains("a"); }
         }).count();
 
-        System.out.println("Lines with a: " + numAs);
+        System.out.println("Lines with a: " + numAs);*/
     }
 
-
-
-
-
+    private static void calculateLocalTime(String dateTime, String stringOffset) {
+        int offset = Integer.parseInt(stringOffset);
+        Date temp;
+        try {
+            temp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(dateTime);
+            System.out.print("TIME IS: ");
+            System.out.print(temp);
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(temp);
+            cal.add(Calendar.MINUTE, offset);
+            System.out.println("LOCAL TIME IS: ");
+            System.out.print(cal.getTime());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
 }
